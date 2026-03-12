7a4b555f23219e25753ce25d908fb3633e711873
raameshb <raameshb@proton.me>
2026-03-11 22:03:28 -0500

    Add multi-ssm capability
    Signed-off-by: raameshb <raameshb@proton.me>

--- src/generic_ssm.py
+++ src/generic_ssm.py
@@ -11,17 +11,20 @@ from naive_kernel import naive_kernel
 
 
 class SSMLayer(nnx.Module):
-    def __init__(self, rngs, N):
+    def __init__(self, rngs, N, H):
         lecun_n = lecun_normal()
         delta_init = log_step_initializer()
-        self.A = nnx.Param(lecun_n(rngs.params(), (N, N)))
-        self.B = nnx.Param(lecun_n(rngs.params(), (N, 1)))
-        self.C = nnx.Param(lecun_n(rngs.params(), (1, N)))
-        self.D = nnx.Param(jnp.ones((1,)))
-        self.log_step = nnx.Param(jnp.array([delta_init(rngs.params(), (1,))]))
+        self.As = nnx.Param(lecun_n(rngs.params(), (H, N, N)))
+        self.Bs = nnx.Param(lecun_n(rngs.params(), (H, N, 1)))
+        self.Cs = nnx.Param(lecun_n(rngs.params(), (H, 1, N)))
+        self.Ds = nnx.Param(jnp.ones((H,)))
+        self.log_steps = nnx.Param(jnp.array(delta_init(rngs.params(), (H,))))
 
-    def __call__(self, u, use_cache=False, cache_state=True):
-        step = jnp.exp(self.log_step)
-        Ab, Bb, Cb = discretize(self.A, self.B, self.C, step)
+    def ssm(u, log_step, A, B, C, D):
+        step = jnp.exp(log_step)
+        Ab, Bb, Cb = discretize(A, B, C, step)
         K = naive_kernel(Ab, Bb, Cb, u.shape[0])
-        return casual_convolve(u, K)
+        return casual_convolve(u, K) + D * u
+
+    def __call__(self, inp):
+        return nnx.vmap(SSMLayer.ssm, in_axes=(1, 0, 0, 0, 0, 0), out_axes=1)(inp, self.log_steps, self.As, self.Bs, self.Cs, self.Ds)
--- tests/test_ssm.py
+++ tests/test_ssm.py
@@ -3,9 +3,10 @@ from flax import nnx
 
 from generic_ssm import SSMLayer
 
-
 def test_generic_ssm():
+    N = 16
+    H = 8
     rngs = nnx.Rngs(0)
-    model = SSMLayer(rngs, 16)
-    test_inp = jrand.uniform(rngs.random(), (64,))
+    model = SSMLayer(rngs, N, H)
+    test_inp = jrand.uniform(rngs.random(), (64,H))
     model(test_inp)
