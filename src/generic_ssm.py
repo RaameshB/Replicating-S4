fb848fb7f8c06bc778854a95f894e1b0e51586f4
raameshb <raameshb@proton.me>
2026-03-12 00:44:20 -0500

    add in auto batching and multi-ssm capabilities to the SSM layer
    Signed-off-by: raameshb <raameshb@proton.me>

--- src/generic_ssm.py
+++ src/generic_ssm.py
@@ -2,11 +2,9 @@
 import jax.numpy as jnp
 from flax import nnx
 from flax.nnx.nn.initializers import lecun_normal
-
 from convolutional_ssm import casual_convolve
 from init_ssm import discretize, log_step_initializer
 from naive_kernel import naive_kernel
-
 # %%
 
 
@@ -26,5 +24,20 @@ class SSMLayer(nnx.Module):
         K = naive_kernel(Ab, Bb, Cb, u.shape[0])
         return casual_convolve(u, K) + D * u
 
+
     def __call__(self, inp):
-        return nnx.vmap(SSMLayer.ssm, in_axes=(1, 0, 0, 0, 0, 0), out_axes=1)(inp, self.log_steps, self.As, self.Bs, self.Cs, self.Ds)
+        unbatched = len(inp.shape) < 3
+        if (unbatched):
+            inp = inp[:, jnp.newaxis]
+
+        outs = nnx.vmap(lambda unbatched:
+                nnx.vmap(SSMLayer.ssm, in_axes=(1, 0, 0, 0, 0, 0), out_axes=1)(unbatched, self.log_steps.value, self.As.value, self.Bs.value, self.Cs.value, self.Ds.value)
+            )(inp)
+
+        if (unbatched):
+            return outs[0]
+
+        return outs
+# %%
+
+# %%
--- tests/test_ssm.py
+++ tests/test_ssm.py
@@ -3,10 +3,19 @@ from flax import nnx
 
 from generic_ssm import SSMLayer
 
-def test_generic_ssm():
+def test_generic_ssm_unbatched():
     N = 16
     H = 8
     rngs = nnx.Rngs(0)
     model = SSMLayer(rngs, N, H)
     test_inp = jrand.uniform(rngs.random(), (64,H))
+    model(test_inp)
+
+def test_generic_ssm_batched():
+    N = 16
+    H = 8
+    batch = 16
+    rngs = nnx.Rngs(0)
+    model = SSMLayer(rngs, N, H)
+    test_inp = jrand.uniform(rngs.random(), (batch, 64, H))
     model(test_inp)
